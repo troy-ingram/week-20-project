a91ad263bf1f45a91f3d534c3c251e7fdc491794
EC2 Default User <ec2-user@ip-172-31-19-32.ec2.internal>
2021-06-21 10:39:57 +0000

    update TC variables

--- variables.tf
+++ variables.tf
@@ -1,9 +1,13 @@
 # --- root/variables.tf ---
 
-variable "aws_region" {
-  default = "us-east-1"
-}
+# variable "aws_region" {
+#   default = "us-east-1"
+# }
 
 variable "access_ip" {
   type = string
 }
+
+variable "ACCESS_KEY" {}
+variable "SECRET_ACCESS_KEY" {}
+variable "REGION" {}
